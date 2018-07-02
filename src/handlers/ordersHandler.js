315850a7a036161826b05a3901a5ffa4b9ccf7b7
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-07-02 11:43:12 -0400

    adding json validators for request

---
+++ src/handlers/ordersHandler.js
@@ -0,0 +1,4 @@
+import { Client } from 'pg'
+export const postOrder = (request, response, next) => {
+  console.log('req.body', req.body)
+}
---
+++ src/validators/index.js
@@ -0,0 +1,10 @@
+import Joi from 'joi'
+
+const UUID_REGEX = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/
+const LIMIT_AMOUMT = 99999999
+
+export const credentialsSchema = Joi.object().keys({
+  email: Joi.string().required(),
+  name: Joi.string().required(),
+  password: Joi.string().required()
+})
