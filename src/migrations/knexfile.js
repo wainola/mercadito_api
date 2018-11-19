b30669db9c036d04b7ba3287e52dbe11e49a4f48
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-11-19 20:48:28 -0300

    updating dependencies

--- src/migrations/knexfile.js
+++ src/migrations/knexfile.js
@@ -6,36 +6,4 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL
   }
-
-  // staging: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // },
-
-  // production: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // }
 };
