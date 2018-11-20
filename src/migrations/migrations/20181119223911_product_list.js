f9a6258b8a6bad22b6f9830011f5abfe1d253295
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-11-19 22:56:22 -0300

    fixing error on migrations

---
+++ src/migrations/migrations/20181119223911_product_list.js
@@ -0,0 +1,14 @@
+exports.up = async knex => {
+  await knex.schema.raw(`
+  CREATE TABLE product_list(
+    id uuid not null primary key default gen_random_uuid(),
+    id_inventory uuid references inventory(id) not null,
+    name text not null,
+    create_at timestamp default now()
+  )
+  `);
+};
+
+exports.down = async knex => {
+  await knex.schema.dropTableIfExists("product_list");
+};
