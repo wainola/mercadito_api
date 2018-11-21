4d18590973ed52fe806d10111004efdde8ed91f0
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-11-20 22:42:42 -0300

    WIP: schema validators

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,5 +1,5 @@
 // import Joi from 'joi'
-const Joi = require('joi');
+const Joi = require("joi");
 
 const UUID_REGEX = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/;
 const LIMIT_AMOUMT = 99999999;
@@ -7,7 +7,9 @@ const LIMIT_AMOUMT = 99999999;
 exports.credentialsSchema = Joi.object().keys({
   email: Joi.string().required(),
   name: Joi.string().required(),
-  password: Joi.string().min(7).required(),
+  password: Joi.string()
+    .min(7)
+    .required()
 });
 
 exports.productSchema = Joi.object().keys({
@@ -15,5 +17,26 @@ exports.productSchema = Joi.object().keys({
   urlImage: Joi.string().required(),
   description: Joi.string().required(),
   stock: Joi.string().required(),
-  type: Joi.string().required(),
+  type: Joi.string().required()
 });
+
+exports.categoriesSchema = Joi.object().keys({
+  id: Joi.string()
+    .uuid()
+    .required(),
+  name: Joi.string().required()
+});
+
+exports.clientSchema = Joi.object().keys({
+  id: Joi.string()
+    .uuid()
+    .required(),
+  name: Joi.string().required(),
+  lastname: Joi.string().required(),
+  email: Joi.string()
+    .email()
+    .required(),
+  address: Joi.string().required()
+});
+
+exports.orderHistorySchema = Joi.object().keys({});
