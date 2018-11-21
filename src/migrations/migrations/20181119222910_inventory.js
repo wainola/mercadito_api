ab33704b224bede734c7746d1e15d8bd3fad6e9c
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-11-20 21:34:34 -0300

    finishing migrations

--- src/migrations/migrations/20181119222910_inventory.js
+++ src/migrations/migrations/20181119222910_inventory.js
@@ -1,6 +1,6 @@
 exports.up = async knex => {
   await knex.schema.raw(`
-  CREATE TABLE invetory(
+  CREATE TABLE inventory(
     id uuid not null primary key default gen_random_uuid(),
     id_categories uuid references categories(id) not null,
     product_name text not null,
@@ -8,7 +8,8 @@ exports.up = async knex => {
     price text not null,
     type text not null,
     description text not null,
-    last_update timestamp
+    last_update timestamp not null,
+    created_at timestamp default now()
   )
   `);
 };
--- src/migrations/migrations/20181119224200_order.js
+++ src/migrations/migrations/20181119224200_order.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = async knex => {
+  await knex.schema.raw(`
+  CREATE TABLE order(
+    id uuid not null primary key default gen_random_uuid(),
+    id_client uuid references client(id) not null,
+    id_order_history uuid references order_history(id) not null,
+    created_at timestamp default now()
+  )
+  `);
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = async knex => {
+  await knex.schema.dropTableIfExists("order");
 };
