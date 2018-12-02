780f6a0372391e2410eb356dede4580bfc1be801
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-12-02 19:47:39 -0300

    WIP: order handlers

--- src/handlers/ordersHandler.js
+++ src/handlers/ordersHandler.js
@@ -1,21 +1,21 @@
-const Joi = require('joi');
-const { credentialsSchema } = require('../validators/index');
+const Joi = require("joi");
+const { credentialsSchema } = require("../validators/index");
 
-exports.postOrder = (request, response) => {
+exports.postOrder = async (request, response) => {
   let reply;
   const isValidJSON = Joi.validate(request.body, credentialsSchema);
   if (isValidJSON !== null) {
     reply: {
       error: {
-        message: 'Malformed json'
+        message: "Malformed json";
       }
-    };
+    }
 
-    return response.status(422).send(reply);
+    return await response.status(422).send(reply);
   }
 
   const { order } = request.body;
-  const insertIntoOrders = '';
+  const insertIntoOrders = `INSERT INTO order`;
 
   return response.status(201).send(reply);
-}
+};
