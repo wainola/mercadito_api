e081b12951ad70800c810c11f195d27f5b2b2b93
Nicolás Riquelme <nrriquelme@gmail.com>
2019-07-20 16:45:55 -0400

    Middleware to validate JSON request body

--- src/middlewares/validateJSONBody.js
+++ src/middlewares/validateJSONBody.js
@@ -7,12 +7,39 @@ const methodsRequest = ['post'];
 const mapUrl = url => appUrls.filter(itemUrl => itemUrl === url);
 const methodToLowerCase = method => method.toLowerCase();
 const mapMethodRequest = methodRequest => methodsRequest.filter(method => method === methodRequest);
+const genStructure = obj => ({
+  criteriaTuple: [...obj.url, ...obj.method],
+  body: obj.body
+});
+const validateByUrlandMethod = ({ criteriaTuple: [url, method], body }) => {
+  switch (url) {
+    case '/category':
+      if (method === 'post') {
+        const validationResult = Joi.validate(body, categoryValidators.postSchema);
+        return validationResult;
+      }
+    default:
+      return null;
+  }
+};
 
 // validateJSONBody :: Strgin -> String -> Object
-const validateJSONBody = (url, method) =>
-  Promise.resolve({ url: mapUrl(url), method: methodToLowerCase(method) }).then(obj => ({
-    ...obj,
-    method: mapMethodRequest(obj.method)
-  }));
+const validateJSONBody = (url, method, body) =>
+  Promise.resolve({ url: mapUrl(url), method: methodToLowerCase(method), body })
+    .then(dataProcessed => ({
+      ...dataProcessed,
+      method: mapMethodRequest(dataProcessed.method),
+      body
+    }))
+    .then(obj => genStructure(obj))
+    .then(objectStructured => ({ error: validateByUrlandMethod(objectStructured).error }));
 
-module.exports = validateJSONBody;
+const JSONValidatorMiddleware = async (request, response, next) => {
+  const { error } = await validateJSONBody(request.url, request.method, request.body);
+  if (error !== null) {
+    return response.status(422).send({ msg: 'Malformed JSON' });
+  }
+  return next();
+};
+
+module.exports = JSONValidatorMiddleware;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
-const validateJSONBody = require('../middlewares/validateJSONBody');
+const JSONValidatorMiddleware = require('../middlewares/validateJSONBody');
 
 const router = express.Router();
 
@@ -13,15 +13,9 @@ const client = new Pool({ connectionString: DATABASE_URL });
 
 const categoryHandler = new CategoryHandler(client);
 
-const someMiddleware = async (request, response, next) => {
-  const r = await validateJSONBody(request.url, request.method);
-  console.log('r:', r);
-  return next();
-};
-
 // CATEGORIES
 // router.get('/categories', categoryHandler.getCategories);
-router.post('/category', someMiddleware, categoryHandler.postCategory);
+router.post('/category', JSONValidatorMiddleware, categoryHandler.postCategory);
 // router.delete('/categories', categoryHandler.deleteCategory);
 
 module.exports = router;
