ef63ba3979f9c4d7f6a65336e9eb5006974e236a
Nicolás Riquelme <nrriquelme@gmail.com>
2019-07-20 18:10:44 -0400

    Categories handler test updated

--- src/__tests__/handlers/CategoryHandler.test.js
+++ src/__tests__/handlers/CategoryHandler.test.js
@@ -21,26 +21,49 @@ class Response {
 describe('CategoryHandler', () => {
   let res;
   let categoryHandler;
+  let postCategorySpy;
+  let updateCategorySpy;
+  let deleteCategorySpy;
+  let getCategoriesSpy;
+  let postCategoryBody;
+  let updateCategoryBody;
+  let deleteCategoryBody;
   beforeAll(() => {
     res = new Response();
     categoryHandler = new CategoryHandler();
-  });
-  it('should return 422 if passed the wrong body', async () => {
-    const categorytoSend = {
-      category: {
-        category_name: false
-      }
+    postCategorySpy = jest.spyOn(categoryHandler, 'postCategory');
+    updateCategorySpy = jest.spyOn(categoryHandler, 'updateCategory');
+    deleteCategorySpy = jest.spyOn(categoryHandler, 'deleteCategory');
+    getCategoriesSpy = jest.spyOn(categoryHandler, 'getCategories');
+
+    postCategoryBody = {
+      body: { category: { category_name: 'some name' } }
     };
-
-    const req = {
-      body: categorytoSend
+    updateCategoryBody = {
+      body: { category: { id: '1', category_name: 'some name' } }
     };
-
-    const r = await categoryHandler.postCategory(req, res);
-    expect(r.statusCode).toBe(422);
+    deleteCategoryBody = {
+      body: { category: { id: '1' } }
+    };
   });
 
-  it('should return 200 if passed the correct body', async () => {
+  it('should call the postCategory method', async () => {
+    await categoryHandler.postCategory(postCategoryBody, res);
+    expect(postCategorySpy).toHaveBeenCalled();
+  });
+  it('should call the updateCategory method', async () => {
+    await categoryHandler.updateCategory(updateCategoryBody, res);
+    expect(updateCategorySpy).toHaveBeenCalled();
+  });
+  it('should call the deleteCategory method', async () => {
+    await categoryHandler.deleteCategory(deleteCategoryBody, res);
+    expect(deleteCategorySpy).toHaveBeenCalled();
+  });
+  it('should call the getCategories method', async () => {
+    await categoryHandler.getCategories({}, res);
+    expect(getCategoriesSpy).toHaveBeenCalled();
+  });
+  it('should return 200 when passed the body', async () => {
     const categoryToSend = {
       category: {
         category_name: 'tallarines'
