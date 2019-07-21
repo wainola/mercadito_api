27dca38c7aee70cc7f9fe80d9239e1869a653335
Nicolás Riquelme <nrriquelme@gmail.com>
2019-07-21 16:01:25 -0400

    Stock model class

---
+++ src/models/StockModel/index.js
@@ -0,0 +1,11 @@
+class Stock extends Database {
+  async buildQuery(type, data, id) {}
+
+  async insertStock() {}
+
+  async updateStock() {}
+
+  async deleteStock() {}
+
+  async getStocks() {}
+}
