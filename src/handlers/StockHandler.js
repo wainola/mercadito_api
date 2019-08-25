d93738878b66db4e3f2421d5e96a5b0a8cac208b
Nicolás Riquelme <nrriquelme@gmail.com>
2019-08-25 13:50:53 -0400

    Stock handler

--- src/handlers/StockHandler.js
+++ src/handlers/StockHandler.js
@@ -1,1 +1,32 @@
-const Base = require('./BaseHandler');
+const MetaApeiron = require('meta-apeiron');
+const StockModel = require('../models/StockModel');
+
+class StockHandler {
+  constructor(client = null) {
+    this.stockModel = MetaApeiron.setDependency(new StockModel(client)).proxyInstance();
+  }
+
+  async postStockQuantity({ body }, response) {
+    const {
+      stock: { quantity }
+    } = body;
+    const stockInserted = await this.stockModel.insert(quantity);
+    return response.status(200).send({ data: stockInserted });
+  }
+
+  async updateStock({ body }, response) {
+    const {
+      stock: { quantity, id }
+    } = body;
+    const stockUpdated = await this.stockModel.update(quantity, id);
+    return response.status(200).send({ data: stockUpdated });
+  }
+
+  async getStock({ body }, response) {
+    const {
+      stock: { quantity, id }
+    } = body;
+    const selectedStock = await this.stockModel.get(quantity, id);
+    return response.status(200).send({ data: selectedStock });
+  }
+}
